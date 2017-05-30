342d1ff091e0ce3670a52e375114f5cd10e37efa
chassy33 <chassy33@gmail.com>
2017-05-30 11:15:21 -0500

    Add files via upload

---
+++ lesson4/lesson4.js
@@ -0,0 +1,23 @@
+document.body.style.backgroundColor = "red";
+
+document.querySelector("h1").style.backgroundColor = "blue";
+
+document.body.style.fontFamily = "sans-serif";
+
+document.getElementById("nickname").innerHTML = "Chassy";
+
+document.getElementById("favorites").innerHTML = "Cars, games, traveling";
+
+document.getElementById("hometown").innerHTML = "Haven, KS";
+
+var items = document.getElementsByTagName('li');
+   for (var i = 0; i < items.length; i++) {
+      items[i].className = 'listitem';
+      items[i].style.color = "red";
+      items[i].style.backgroundColor = "white";
+   }
+
+var image = document.createElement("img");
+pic.src = "self_image.jpg";
+
+document.body.appendChild(img);
